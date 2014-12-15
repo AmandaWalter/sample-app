c6e831c09a0acdecf0413abc3a6230defc412a55
Amanda Walter <anhart@gmail.com>
2014-12-15 12:57:29 -0700

    Updates to admin priv and security issues

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ source 'https://rubygems.org'
 gem 'rails'
 gem 'bcrypt',               '3.1.7'
 gem 'bootstrap-sass'
+gem 'faker',                '1.4.2'
+gem 'will_paginate',           '3.0.7'
+gem 'bootstrap-will_paginate', '0.0.10'
 # Use postgresql as the database for Active Record
 gem 'pg'
 # Use SCSS for stylesheets
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,10 +1,19 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-	def setup
+	
+    def setup
     @user = users(:michael)
     end
-  
+  test "login with invalid information" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, session: { email: "", password: "" }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
     test "login with valid information followed by logout" do
         get login_path
         post login_path, session: { email: @user.email, password: 'password' }
@@ -27,8 +36,9 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     end
     test "login with remembering" do
         log_in_as(@user, remember_me: '1')
-        assert_not_nil cookies['remember_token']
+        assert_equal assigns(:user).remember_token, cookies['remember_token']
     end
+    
     test "login without remembering" do
         log_in_as(@user, remember_me: '0')
         assert_nil cookies['remember_token']
